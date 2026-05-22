79be230bc252af8a3b3b71bee1340de0ece2e840
Safwaan <hellosafwaan@gmail.com>
2026-05-22 18:03:33 +0530

    feat: design-system/textarea-component

--- design-system/textarea-component/js/index.js
+++ design-system/textarea-component/js/index.js
@@ -1,2 +1,12 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const textAreaDefault = document.querySelector("#textarea-default")
+const textareaCounter = document.querySelector('.textarea-group__character-count')
+textAreaDefault.addEventListener('input', (event) => {
+  const characterCount = event.target.value.length;
+  textareaCounter.textContent = `${characterCount}/500`
+  let textAreaGroup = event.target.closest('.textarea-group')
+  if(characterCount > 500) {
+    textAreaGroup.classList.add('textarea-group--exceeded');
+  } else {
+    textAreaGroup.classList.remove('textarea-group--exceeded');
+  }
+})
