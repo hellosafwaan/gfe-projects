adaf4f4bec8678cc5dee49160e84ef782d4696ae
Safwaan <hellosafwaan@gmail.com>
2026-05-29 18:19:25 +0530

    feat: design-system/toast-component

--- design-system/toast-component/js/index.js
+++ design-system/toast-component/js/index.js
@@ -1,2 +1,15 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+function showToast(duration = 7500) {
+    const toastElement = document.querySelector("#toast-demo");
+    toastElement.classList.add('show')
+
+    setTimeout(() => {
+        toastElement.classList.remove('show');
+        toastElement.classList.add('hide');
+    }, duration)
+
+    toastElement.addEventListener('transitionend', () => {
+        if (toastElement.classList.contains('hide')) {
+            toastElement.classList.remove('hide');
+        }
+    });
+}
