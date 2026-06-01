1a5d83dc9d293fcb990e72d9a5bedfa3afd9f604
Safwaan <hellosafwaan@gmail.com>
2026-06-01 18:33:35 +0530

    feat: design-system/tab-menu-component

--- design-system/tab-menu-component/js/index.js
+++ design-system/tab-menu-component/js/index.js
@@ -1,2 +1,14 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const tabsListElement = document.querySelector('.tabs__list');
+
+tabsListElement.addEventListener('click', (event) => {
+    let activeTab = document.querySelector('.tabs__trigger--active');
+    const item = event.target.closest('.tabs__trigger');
+    if(!item || item === activeTab) return;
+    if (activeTab) activeTab.classList.remove('tabs__trigger--active')
+    item.classList.add('tabs__trigger--active');
+
+    const currentTabValue = item.dataset.tab
+    let currentPanelElement = document.querySelector('.tabs__panel--active');
+    currentPanelElement?.classList.remove('tabs__panel--active');
+    document.getElementById(`${currentTabValue}-panel`).classList.add('tabs__panel--active')
+})
